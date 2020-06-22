87357b06fc917ddaac7858aadb994870727db2f1
Marie Quintas <quintasmarie@gmail.com>
2020-06-22 12:28:31 +0200

    Ajout des films/modification/suppression

--- src/config/routes.rb
+++ src/config/routes.rb
@@ -1,17 +1,23 @@
 Rails.application.routes.draw do
+  resources :movies
   devise_for :users
   resources :categories
   resources :comments
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :posts # localhost:3000/posts
 
+  #Routes gestion erreur
+  get '/errors', to: 'errors#index'
+
   #Route / ou /home
   get '/home', '/', to: 'home#index'
-  
-  #Route pour les films
-  get '/movies', to: 'movies#index'
-  get '/movies/add', to: 'movies/add'
-  get '/movies/show', to: 'movies/show'
 
-  
+  #Route pour les films
+  #get '/movies', to: 'movies#index'
+  #get '/movies/new', to: 'movies#new'
+  #get '/movies/show', to: 'movies#show'
+
+  #Route backoffice Admin
+  get '/users', to: 'users#index'
+
 end
--- src/db/schema.rb
+++ src/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_27_203839) do
+ActiveRecord::Schema.define(version: 2020_06_21_230330) do
 
   create_table "categories", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
     t.string "name"
@@ -50,6 +50,7 @@ ActiveRecord::Schema.define(version: 2020_05_27_203839) do
     t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.boolean "admin"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
